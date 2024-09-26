10cca538079d5196a5a11fda84044eb7dc9c1710
Sivalod <oguho4kav3@gmail.com>
2024-09-26 22:10:47 +0200

    Add files via upload

---
+++ pythonProject/main.py
@@ -0,0 +1,23 @@
+import pygame
+from settings import *
+from player import Player
+import math
+
+pygame.init()
+sc = pygame.display.set_mode((width, height))
+player = Player()
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    player.movement()
+    sc.fill(black)
+
+    pygame.draw.circle(sc, green, player.pos, 12)
+    pygame.draw.line(sc, red, player.pos, (player.x + width * math.cos(player.angle),
+                                                   player.y + height * math.sin(player.angle)))
+
+    pygame.display.flip()
+    clock.tick(FPS)
---
+++ pythonProject/settings.py
@@ -0,0 +1,21 @@
+# game settings
+width = 1200
+height = 800
+half_width = width // 2
+half_height = height // 2
+FPS = 90
+
+# player settings
+player_pos = (half_width, half_height)
+player_angle = 0
+player_speed = 2
+
+# colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (220, 0, 0)
+green = (0, 220, 0)
+blue = (0, 0, 220)
+dark_gray = (110, 110, 110)
+purple = (120, 0, 120)
+
